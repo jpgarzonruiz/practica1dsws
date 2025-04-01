614aa521b6630f45e7fa19442f93b10893ae7248
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-04-01 16:43:31 -0500

    a

--- backend/controllers/estudiantescontrollers.js
+++ backend/controllers/estudiantescontrollers.js
@@ -7,38 +7,13 @@ class EstudiantesController{
     }
 
     consultar(req,res){
-        try{/*
-            db.query('SELECT  * FROM estudiantes',
-            [],(err,rows) => {
-                if(err) {
-                    res.status (400).send(err.message);
-                }
-                res.status(200).json(rows);
-            });*/
+        try{
             res.status (200).send ("Funciono ok");
         }catch (err){
             res.status(500).send(err.message);
         }
     }
-/*
-    actualizar(req,res){
-        const {id} = req.params;
-        try{
-            const {dni,nombre,apellidos,email} = req.body;
-            db.query('UPDATE cursos.estudiantes SET dni=?, nombre=?, apellidos=?, email=? WHERE id=?;',
-            [dni,nombre,apellidos,email,id],(err,rows) => {
-                if(err) {
-                    res.status (400).send(err.message);
-                }
-                if (rows.affectedRows == 1)
-                    res.status(200).json({respuesta:"Registro actualizado correctamente"});
-            });
-        }catch (err){
-            //console.log(err);
-            res.status(500).send(err.message);
-        }
-    }
-*/
+
     ingresar(req,res){
         try{
             const myJSON = JSON.stringify(req.body);
@@ -47,15 +22,7 @@ class EstudiantesController{
 
             const {dni,nombre,apellidos,email} = req.body;
             console.log ("el dni que llega es de " + dni);
-/*
-            db.query('INSERT INTO estudiantes (id, dni, nombre, apellidos, email) VALUES (NULL, ?, ?, ?, ?);',
-            [dni,nombre,apellidos,email],(err,rows) => {
-                if(err) {
-                    res.status (400).send(err.message);
-                }else{
-                    res.status(201).json({id: rows.insertId});
-                }
-            });*/
+
             res.status(201).json({id: "123");
         }catch (err){
             res.status(500).send(err.message);
@@ -78,23 +45,7 @@ class EstudiantesController{
         }
 
     }
-/*
-    borrar(req,res){
-        const {id} = req.params;
-        try{
-            req.body;
-            db.query('DELETE FROM cursos.estudiantes WHERE id=?;',
-            [id],(err,rows) => {
-                if(err) {
-                    res.status (400).send(err.message);
-                }
-                if (rows.affectedRows == 1)
-                    res.status(200).json({respuesta:"Registro borrado correctamente"});
-            });
-        }catch (err){
-            res.status(500).send(err.message);
-        }
-   }*/
+
 }
 
 module.exports = new EstudiantesController();
