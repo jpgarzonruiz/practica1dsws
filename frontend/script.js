88077bdeec9f5f6d5b1690d20d0bdc55e71bdcac
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-04-01 18:43:09 -0500

    asasas

--- frontend/script.js
+++ frontend/script.js
@@ -22,7 +22,7 @@ function guardar(){
       redirect: "follow"
     };
 
-    fetch("https://ejemplodedsws.netlify.app/estudiantes", requestOptions)
+    fetch("https://ejemplodedsws.netlify.app/.netlify/functions/estudiantes", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.error(error));
