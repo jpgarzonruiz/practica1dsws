d1070da91101cdbe81226f201c620ac77cbb6b87
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-04-01 21:09:16 -0500

    asass

--- frontend/script.js
+++ frontend/script.js
@@ -71,8 +71,8 @@ function listar(){
     };
     fetch("https://ejemplodedsws.netlify.app/.netlify/functions/webService", requestOptions)
       .then((response) =>
-        //response.text())
-        console.log("la respuesta es de " + response.text))
+        response.text())
+        //console.log("la respuesta es de " + response.text))
       .then((result) =>
         //cargar(result))
         console.log("esto aa es lo que se devuelve es decir nada:"))
