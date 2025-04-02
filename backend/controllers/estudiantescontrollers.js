fcd24d62c2a9cc4aee7a5e721e10336a934b4c2d
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-04-02 04:08:15 -0500

    con arreglos

--- backend/controllers/estudiantescontrollers.js
+++ backend/controllers/estudiantescontrollers.js
@@ -1,11 +1,19 @@
 class EstudiantesController{
+    let arreglo=[];
+
     construct(){
     }
 
     consultar(req,res){
         try{
             let myObj = {dni: "1234", nombre: "Juan", apellidos: "Perez", email:"ejemplo@nose.com"};
-            let myJSON = JSON.stringify(myObj);
+            arreglo.push (myObj); //nuevo
+
+            let myObj2 = {dni: "2", nombre: "J2uan", apellidos: "222Perez", email:"222ejemplo@nose.com"};
+            arreglo.push (myObj2); //nuevo
+
+            let myJSON = JSON.stringify(arreglo);
+            //let myJSON = JSON.stringify(myObj);
             res.status(200).send (myJSON);
         }catch (err){
             res.status(500).send(err.message);
