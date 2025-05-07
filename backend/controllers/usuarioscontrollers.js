0ab301325abb45e4df04b0a0847fefefc7b25ffa
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-05-07 18:26:56 -0500

    kk

--- backend/controllers/usuarioscontrollers.js
+++ backend/controllers/usuarioscontrollers.js
@@ -32,7 +32,7 @@ class UsuariosController{
             console.log ("email: "+ email);
             */
 
-            const docRef = await admin.firestore().collection('users').add(req.body);
+            const docRef = await admin.firestore().collection('users').doc("user654").add(req.body);
 
             res.status(200).send ("Usuario agregado");
         }catch (err){
