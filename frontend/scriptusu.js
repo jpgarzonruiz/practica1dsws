86a9723e895cea2a5413f9a69863ccb92def7c36
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-05-07 14:12:27 -0500

    nb

--- frontend/scriptusu.js
+++ frontend/scriptusu.js
@@ -1,3 +1,67 @@
+function guardar(){
+
+    let apellidos='';
+    let datoingresado = document.getElementById("correo").value;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    event.preventDefault();
+
+    let raw = JSON.stringify({
+      "dni": document.getElementById("dni").value,
+      "nombre": document.getElementById("nombre").value,
+      "apellidos": document.getElementById("apellidos").value,
+      "email": document.getElementById("correo").value
+    });
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("https://ejemplodedsws.netlify.app/.netlify/functions/estudiantes", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+}
+
+//Ejemplo cuando se devuelve algo
+function cargar(resultado){
+    let transformado = JSON.parse(resultado);
+    var salida="";
+    var elemento="";
+
+
+    for (let vc in transformado){
+        elemento =  "<br>DI: " + transformado[vc].dni;
+        elemento = elemento + "<br>Nombres y apellidos: " + transformado[vc].nombre + " " + transformado[vc].apellidos;
+        elemento = elemento + "<br>Correo electrónico: " + transformado[vc].email;
+        salida = salida  + elemento + "<br><br>";
+    }
+
+    document.getElementById("rta").innerHTML = salida;
+}
+
+function listar(){
+    event.preventDefault();
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    fetch("https://ejemplodedsws.netlify.app/.netlify/functions/estudiantes", requestOptions)
+      .then((response) =>
+        response.text())
+      .then((result) =>
+        cargar(result))
+      .catch((error) =>
+        console.error(error));
+}
+
+
+/*
+
 
 //Ejemplo cuando se devuelve algo
 function cargar(resultado){
@@ -17,6 +81,8 @@ function cargar(resultado){
 }
 
 function oka(){
+
+*/
     /*
     document.getElementById('consultar').addEventListener('click', async () => {
       //const userId = 'user124'; // Reemplaza con el ID del documento que agregaste
@@ -33,6 +99,7 @@ function oka(){
       }
     });
     */
+/*
     const userId = 'user124';
     event.preventDefault();
     const requestOptions = {
@@ -67,3 +134,4 @@ function listar(){
       .catch((error) =>
         console.error(error));
 }
+*/
