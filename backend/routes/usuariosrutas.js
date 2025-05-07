06f9bf5922eef3f2d65bc9a80e13bf9a46214a1a
jpgarzonruiz <juanpablo.garzonruiz@gmail.com>
2025-05-06 20:31:05 -0500

    a4

--- backend/routes/usuariosrutas.js
+++ backend/routes/usuariosrutas.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const usuarioscontroller = require("../controllers/usuarioscontrollers.js");
-
+/*
 router.get("/",usuarioscontroller.consultar);
 router.post("/",usuarioscontroller.ingresar);
-
+*/
 /* Ejemplo con parámetros */
 router.route("/:id")
 .get(usuarioscontroller.consultarDetalle)
+/*
 .put(usuarioscontroller.actualizar)
 .delete(usuarioscontroller.borrar);
-
+*/
 
 module.exports = router;
